33698a37e71b176cf4af046bf31f5bde9daaf2dc
anacperdigao <anacarolinaperdigao1@gmail.com>
2023-01-08 15:59:05 -0300

    refactor: refactoring app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
-// importando o framework express
+// Importando o framework express
 import express from "express";
 
-// fazendo a conexao com o mongodb
+// Criando uma instância do express
+const app = express();
+
+// Fazendo a conexao com o mongodb
 import db from "./config/dbConnect.js";
 db.on("error", console.log.bind(console, 'Erro de conexão'))
 
@@ -10,56 +13,10 @@ db.once("open", () => {
     console.log('Conexão com o banco feita com sucesso.')
 })
 
-// Criando uma instância do express
-const app = express();
+// Aqui vou importar as rotas
+import routes from "./routes/index.js";
+routes(app)
 
-// Isso vai fazer interpretar o que eu for mandar via post e via put como json
-app.use(express.json())
-
-// Fiz uma const só pra poder ter um array com alguns livros
-const livros = [
-    {id: 1, "titulo": "Senhor dos Aneis"},
-    {id: 2, "titulo": "O Hobiit"}
-]
-
-// Aqui é o que eu quero que aconteça quando eu fizer uma requisição para uma determinada rota
-app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node')
-})
-
-app.get('/livros', (req, res) => {
-    res.status(200).json(livros)
-})
-
-app.get('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    res.json(livros[index])
-})
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body)
-    res.status(201).send('Livro foi cadastrado com sucesso')
-})
-
-app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo
-    res.json(livros)
-})
-
-app.delete('/livros/:id', (req, res) => {
-    let { id } = req.params
-    let index = buscaLivro(id);
-    livros.splice(index, 1)
-    res.send(`Livro ${id} removido com sucesso`)
-})
-
-// Função auxiliar dos metodos get por id, put e delete, pq é pra achar o id certo dentro do array
-function buscaLivro (id) {
-    return livros.findIndex(livro => livro.id == id)
-}
 
 // Preciso exportar a instancia do express para poder usar em outro lugar
 export default app
-
-// Depois daqui vou para o arquivo server e modificar lá
