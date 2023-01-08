784ac30fe244b44bfb460be2611230069b4bd21c
anacperdigao <anacarolinaperdigao1@gmail.com>
2023-01-08 15:59:09 -0300

    refactor: refactoring app.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 // O caminho original que eu copiei la do MongoDB foi esse
 // mongodb+srv://anaperdigao:<password>@cursoalura.ndznakm.mongodb.net/?retryWrites=true&w=majority
-mongoose.connect("mongodb+srv://anaperdigao:luzpropria@cursoalura.ndznakm.mongodb.net")
+mongoose.connect("mongodb+srv://anaperdigao:luzpropria@cursoalura.ndznakm.mongodb.net/Alura-node")
 
 let db = mongoose.connection
 
