c23d7ab4413c89f99d00c0052f81432a59d7a649
anacperdigao <anacarolinaperdigao1@gmail.com>
2023-01-08 11:15:30 -0300

    feat: config server

---
+++ notes.md
@@ -0,0 +1,7 @@
+Instalei a lib nodemon pq ele vai escutar toda vez que eu mudar as rotas e vai dar um live reload
+    - npm install nodemon 
+
+Para instalar o framework Express:
+    - npm install express@4.17.3
+
+Tive que adicionar "type": "module" no package.json pra indicar que é JS do tipo module e então poder importar e exportar funções e constantes.
--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 // A principio eu quero criar um servidor local para escutar tudo numa porta e eu fazer as minhas requisições
+// Aqui está a primeira diferença usando o express, porque não vou usar o tipo http, vou apenas importar o que 
+// eu fiz em app.js
+import app from './src/app.js'
+
+// Aqui defino qual a porta que ele vai ficar escutando
+// Já vamos deixar com essa boa prática de diferenciar ou a porta no ambiente de produção ou a porta fixa 3000
+const port = process.env.PORT || 3000
+
+// Aqui vou fazer esse link entre o servidor que criei e a porta que ele vai escutar a requisição e mandar a resposta
+app.listen(port, () => {
+    console.log(`Servidor escutando na porta http://localhost:${port}`)
+})
+
+
+
+
+/*----------------------------------- FORMA ANTERIOR SEM TER O EXPRESS INSTALADO --------------------------------
+// A principio eu quero criar um servidor local para escutar tudo numa porta e eu fazer as minhas requisições
 
 // Crisei uma constante e disse que quero que seja do tipo http
 const http = require("http")  
@@ -6,10 +24,19 @@ const http = require("http")
 // Aqui defino qual a porta que ele vai ficar escutando
 const port = 3000
 
+// Aqui vou criar as rotas
+// Nesse momento eu instalo com npm install nodemon pq ele vai escutar toda vez que eu mudar a rota 
+// e vai dar um live reload@
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Entrei na pagina de livros',
+    '/autores': 'Listagem de autores'
+}
+
 // Aqui de fato criei o servidor e defini o que ele vai escutar e como vai acontecer a comunicação
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Curso de Node')
+    res.end(rotas[req.url])
 })
 
 // Aqui vou fazer esse link entre o servidor que criei e a porta que ele vai escutar 
@@ -17,3 +44,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
     console.log(`Servidor escutando na porta ${port}`)
 })
+*/
